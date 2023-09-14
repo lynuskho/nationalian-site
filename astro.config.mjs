354d541445287d1aca8f4c37cf6dd8a21c4bf989
Lynus Kho <lynusgarrett@gmail.com>
2023-09-14 23:35:49 +0800

    add videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import tailwind from "@astrojs/tailwind";
+import embeds from 'astro-embed/integration';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()]
+  integrations: [embeds(), mdx(), sitemap()]
 });
